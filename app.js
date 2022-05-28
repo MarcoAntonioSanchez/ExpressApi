0d964ecd5b14fac91e03387b4e54e1d2f75d908a
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-27 21:45:30 -0500

    05. Adding the 200 status for a delivery on json

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-// Usando objeto Express
+// Using Express object
 const express = require("express");
 
-// App de Express
+// Express app
 const app = express();
-app.use(express.json()); // Indicamos que usaremos JSON
+app.use(express.json()); // Indicating we are gonna use JSON format
 
-// Puerto en que vamos a ver nuestra app: localhost:3000
+// Port from where we are gonna get to our app: localhost:3000
 const port = 3000;
 
-// Con esto inicializamos esta app
+// Routs
+// HTTP methods: GET, POST, PUT, DELETE
+app.get("/v1/explorers", (req, res) => {
+  console.log(`GET Explorers V1 API ${new Date()}`);
+  const explorer1 = { id: 1, name: "Explorer_1" };
+  const explorer2 = { id: 2, name: "Explorer_2" };
+  const explorer3 = { id: 3, name: "Explorer_3" };
+  const explorers = [explorer1, explorer2, explorer3];
+
+  // HTTP code status:
+  res.status(200).json(explorers);
+});
+
+// Initializig the app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
